da1fe8ec84e20fb5097eef95cf3e4bc2c72d3758
ko-he- <koheiyatsushiro@yahoo.co.jp>
2018-07-01 00:10:50 +0900

    add error handring

--- node/app/app.js
+++ node/app/app.js
@@ -4,8 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var clipRouter = require('./routes/clip');
+
+process.on('uncaughtException', function(err) {
+  console.log(err);
+});
+
 
 var app = express();
 
